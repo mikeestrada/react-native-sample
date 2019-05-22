52bb9a21dd8dee459e51eeadc9fb8f5879ade488
Mike Estrada <estr.mike@gmail.com>
2019-05-21 22:50:01 -0500

    init commit

---
+++ screens/SandboxScreen.js
@@ -0,0 +1,10 @@
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+        <View style={{  flex: 1, justifyContent: "center", alignItems: "center"}}>
+          <Text>Hello, world!</Text>
+        </View>
+    );
+  }
+}
